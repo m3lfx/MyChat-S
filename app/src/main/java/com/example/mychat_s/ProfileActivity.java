97da84980616fb2b28e2f5158dfec9cb43d160e3
rommel dalisay <rommelmdalisay@gmail.com>
2024-09-24 15:59:58 +0800

    first commit

--- app/src/main/java/com/example/mychat_s/ProfileActivity.java
+++ app/src/main/java/com/example/mychat_s/ProfileActivity.java
@@ -1,15 +1,52 @@
 package com.example.mychat_s;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ProfileActivity extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ProfileActivity extends AppCompatActivity {
+    private CircleImageView imageViewCircleProfile;
+    private TextInputEditText editTextUserNameProfile;
+    private Button buttonUpdate;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    FirebaseAuth auth;
+    FirebaseUser firebaseUser;
+    Uri imageUri;
+    boolean imageControl = false;
+
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+    private ActivityResultLauncher<Intent> startActivityIntent;
+
+    String image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +57,79 @@ public class ProfileActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference();
+        auth = FirebaseAuth.getInstance();
+        firebaseUser = auth.getCurrentUser();
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
+
+        imageViewCircleProfile = findViewById(R.id.imageViewCircleProfile);
+        editTextUserNameProfile = findViewById(R.id.editTextUserNameProfile);
+        buttonUpdate = findViewById(R.id.buttonUpdate);
+        startActivityIntent = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                            imageUri = result.getData().getData();
+                            Picasso.get().load(imageUri).into(imageViewCircleProfile);
+                            imageControl = true;
+                        } else {
+                            imageControl = false;
+                        }
+                    }
+                });
+        getUserInfo();
+        imageViewCircleProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageChooser();
+            }
+        });
+
+
+
+    }
+    public void getUserInfo()
+    {
+        reference.child("Users").child(firebaseUser.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                String name = snapshot.child("userName").getValue().toString();
+                image = snapshot.child("image").getValue().toString();
+                editTextUserNameProfile.setText(name);
+
+                if (image.equals("null"))
+                {
+                    imageViewCircleProfile.setImageResource(R.drawable.account);
+                }
+                else
+                {
+                    Picasso.get().load(image).into(imageViewCircleProfile);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+    }
+
+
+
+    public void imageChooser()
+    {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityIntent.launch(intent);
     }
 }
