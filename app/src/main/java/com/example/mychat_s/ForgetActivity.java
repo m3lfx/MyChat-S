159b182dce84ad6485f28a2044f2664b22cc032c
rommel dalisay <rommelmdalisay@gmail.com>
2024-09-24 14:18:15 +0800

    first commit

---
+++ app/src/main/java/com/example/mychat_s/ForgetActivity.java
@@ -0,0 +1,30 @@
+package com.example.mychat_s;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class ForgetActivity extends AppCompatActivity {
+    private TextInputEditText editTextForget;
+    private Button buttonForget;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_forget);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        editTextForget = findViewById(R.id.editTextForget);
+        buttonForget = findViewById(R.id.buttonForget);
+    }
+}
