7e65b458551dfc1945aa6a8e9a663c67920b08d8
rommel dalisay <rommelmdalisay@gmail.com>
2024-09-24 15:47:51 +0800

    first commit

--- app/src/main/java/com/example/mychat_s/MainActivity.java
+++ app/src/main/java/com/example/mychat_s/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.mychat_s;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
+public class MainActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +29,33 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        auth = FirebaseAuth.getInstance();
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.chat_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.action_profile)
+        {
+            startActivity(new Intent(MainActivity.this,ProfileActivity.class));
+        }
+        if (item.getItemId() == R.id.action_signout)
+        {
+            auth.signOut();
+            startActivity(new Intent(MainActivity.this,MyLoginActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
--- app/src/main/java/com/example/mychat_s/MyLoginActivity.java
+++ app/src/main/java/com/example/mychat_s/MyLoginActivity.java
@@ -1,21 +1,33 @@
 package com.example.mychat_s;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MyLoginActivity extends AppCompatActivity {
     private TextInputEditText editTextEmail,editTextPassword;
     private Button buttonSignin,buttonSignup;
     private TextView textViewForget;
+    FirebaseAuth auth;
+    FirebaseUser firebaseUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,5 +43,63 @@ public class MyLoginActivity extends AppCompatActivity {
         buttonSignin = findViewById(R.id.buttonSignin);
         buttonSignup = findViewById(R.id.buttonSignup);
         textViewForget = findViewById(R.id.textViewForget);
+        auth = FirebaseAuth.getInstance();
+
+        buttonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyLoginActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        textViewForget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyLoginActivity.this,ForgetActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                if (!email.equals("") && !password.equals(""))
+                {
+                    signin(email,password);
+                }
+                else
+                {
+                    Toast.makeText(MyLoginActivity.this, "Please enter an email and password.", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+    }
+
+    public void signin(String email,String password)
+    {
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this,new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful())
+                {
+                    Intent intent = new Intent(MyLoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(MyLoginActivity.this, "Sign in is successful.", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MyLoginActivity.this, "Sign in is not successful.", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
     }
 }
